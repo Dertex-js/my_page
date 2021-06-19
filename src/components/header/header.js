80611a56f95a6b6d5e4a2e2a7d4b095ebb024a4a
Dertex-js <ryoamir60@gmail.com>
2021-06-19 23:37:15 +0500

    Structure3

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,11 +6,15 @@ import {NavLink, Route} from "react-router-dom";
 const Header = () => {
     return (
         <div className={s.header}>
-            <img src="https://instagram.fppk1-1.fna.fbcdn.net/v/t51.2885-19/s150x150/69513615_2356087021270625_4423425107783843840_n.jpg?tp=1&_nc_ht=instagram.fppk1-1.fna.fbcdn.net&_nc_ohc=EKLI_c2vidgAX9-IenI&tn=LAA7FV1She_6NKux&edm=ABfd0MgBAAAA&ccb=7-4&oh=47e1554eb8b4e5d5ea84cba014a57569&oe=60D46B4B&_nc_sid=7bff83" alt="logo"/>
-            <div className={s.navbar}>
-                <a href="#">Menu</a>
-                <a href="#">Something1</a>
-                <a href="#">Something2</a>
+            <div className={s.items_center}>
+                <img src="https://instagram.fppk1-1.fna.fbcdn.net/v/t51.2885-19/s150x150/69513615_2356087021270625_4423425107783843840_n.jpg?tp=1&_nc_ht=instagram.fppk1-1.fna.fbcdn.net&_nc_ohc=EKLI_c2vidgAX9-IenI&tn=LAA7FV1She_6NKux&edm=ABfd0MgBAAAA&ccb=7-4&oh=47e1554eb8b4e5d5ea84cba014a57569&oe=60D46B4B&_nc_sid=7bff83" alt="logo"/>
+                <div className={s.navbar}>
+                    <ul>
+                        <li>Menu</li>
+                        <li>Coffee</li>
+                        <li>About us</li>
+                    </ul>
+                </div>
             </div>
         </div>
     );
